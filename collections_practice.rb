119d3be2fc18a89cff960c6cb85102f58317d41b
Alice Brunel <alc.brunel@gmail.com>
2019-07-09 18:22:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,5 +18,9 @@ def swap_elements(array)
   array[0], array[1], array[2] = array[0], array[2], array[1]
 end
 
+def swap_elements_from_to(array,index,specified_index)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
 
 
